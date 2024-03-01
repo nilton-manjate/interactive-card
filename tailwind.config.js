86d887c4542efcdb342a222d7d1b540159e7f293
nilton-manjate <ntnmanjate1@gmail.com>
2024-03-01 09:15:02 +0200

    add main page mobile style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ module.exports = {
         "lightGrayishViolet": "hsl(270, 3%, 87%)",
         "darkGrayishViolet": "hsl(279, 6%, 55%)",
         "veryDarkViolet": "hsl(278, 68%, 11%)",
-        
       },
+
+      backgroundImage: {
+        "mainDesktop": "url('../images/bg-main-desktop.png')",
+        "mainMobile": "url('../images/bg-main-mobile.png')",
+        "cardFront": "url('../images/bg-card-front.png')",
+        "cardBack": "url('../images/bg-card-back.png')",
+      },
+
+      fontFamily: {
+        "space": ["Space Grotesk", "sans-serif"]
+      }
     },
   },
   plugins: [],
